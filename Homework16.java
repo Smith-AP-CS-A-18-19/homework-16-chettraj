d4d81fbfeb402aafdf2d939e9168bc54ee66084c
Rajshree <chettraj@berkeleyprep.org>
2019-04-01 10:47:30 -0400

    rajshree chettiyar

--- Homework16.java
+++ Homework16.java
@@ -6,7 +6,15 @@ public class Homework16 {
 	 * multiplication is repeated addition
 	 */
 	public static int problem1(int a, int b) {
-
+		if (a == 0 || b == 0){
+			return a;
+		}
+		else if (b > 0){
+			return a + problem1(a, b - 1);
+		}
+		else{
+			return -a + problem1(a, b + 1);
+		}
 	}
 
 	/* This problem should calculate and return the value
@@ -16,6 +24,15 @@ public class Homework16 {
 	 * exponentiation is repeated multiplication
 	 */
 	public static int problem2(int a, int b) {
+		if (b == 1){
+			return a;
+		}
+		else if (b < 0){
+			return 1 / problem1(a, problem2(a, b + 1));
+		}
+		else {
+			return problem1(a, problem2(a, b-1));
+		}
 
 	}
 
@@ -27,12 +44,25 @@ public class Homework16 {
 	 * half of the array
 	 */
 	public static int problem3(int[] arr) {
+		if (problem3(arr, 0, arr.length/2) > problem3(arr, arr.length/2, arr.length)){
+			return problem3(arr, arr.length/2, arr.length);
+		}
+		else {
+			return problem3(arr, 0, arr.length/2);
+		}
 
 	}
 
 	private static int problem3(int[] arr, int start, int end) {
+		int min = start;
+		for (int i = 0; i < end - 1; i++){
+			if (arr[i] < arr[min]){
+				min = i;
+			}
+		}
+			return arr[min];
+		}
 
-	}
 
 	/* Recursively find the sum of the digits of
 	 * the given number.
@@ -40,7 +70,12 @@ public class Homework16 {
 	 * and the remaining numbers are num / 10
 	 */
 	public static int problem4(int num) {
-
+		if (num == 0){
+			return 0;
+		}
+		else {
+			return num % 10 + problem4(num / 10);
+		}
 	}
 
 	/* We have bunnies standing in a line,
@@ -56,6 +91,15 @@ public class Homework16 {
 	 * problem5(2) → 5
 	 */
 	 public static int problem5(int bunnies) {
+		 if (bunnies == 0){
+			 return 0;
+		 }
+		 else if (bunnies % 2 == 0){
+			 return 3 + problem5(bunnies - 1);
+		 }
+		 else {
+			 return 2 + problem5(bunnies - 1);
+		 }
 
 	 }
 
@@ -95,5 +139,4 @@ public class Homework16 {
 		 }
 
 	 }
-
 }
